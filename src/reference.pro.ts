4e12522eb60466cddf9b02711d5c28e66a49b655
mango <setsunamango@gmail.com>
2023-12-24 22:15:59 +0800

    feat: add isPlainObject

--- src/reference.pro.ts
+++ src/reference.pro.ts
@@ -1,4 +1,13 @@
-import { isArray } from "./reference";
+import { isArray, isObject } from "./reference";
+
+export type PlainObject = {
+  [key: string]: unknown | undefined;
+}
+
+export function isPlainObject(source: unknown): source is PlainObject {
+  const { getPrototypeOf } = Object;
+  return isObject(source) && getPrototypeOf({}) === getPrototypeOf(source);
+}
 
 /**
  * Narrow source type to `Array` && Check is empty array.
--- test/reference.pro.test.ts
+++ test/reference.pro.test.ts
@@ -1,8 +1,50 @@
 import { describe, expect, it } from "vitest";
 
 import {
-  isNonEmptyArray
+  isPlainObject,
+  isEmptyArray,
+  isNonEmptyArray,
 } from '../index';
+
+describe('isPlainObject', () => {
+  it('should return true if source is a plain object', () => {
+    expect(isPlainObject({})).toBe(true);
+    expect(isPlainObject({
+      2: 1,
+      "a": "b"
+    })).toBe(true);
+  });
+
+  it('should return false if source is array', () => {
+    expect(isPlainObject([])).toBe(false);
+  });
+
+  it('should return false if source special constructor object', () => {
+    expect(isPlainObject(new URLSearchParams())).toBe(false);
+    expect(isPlainObject(new Error())).toBe(false);
+    expect(isPlainObject(new FormData())).toBe(false);
+    expect(isPlainObject(new Date())).toBe(false);
+  });
+});
+
+describe('isEmptyArray', () => {
+  it('should return false if source is a non-empty array', () => {
+    expect(isEmptyArray([1, 2, 3])).toBe(false);
+    expect(isEmptyArray(['a', 'b', 'c'])).toBe(false);
+  });
+
+  it('should return true if source is an empty array', () => {
+    expect(isEmptyArray([])).toBe(true);
+  });
+
+  it('should return false if source is not an array', () => {
+    expect(isEmptyArray(undefined)).toBe(false);
+    expect(isEmptyArray(null)).toBe(false);
+    expect(isEmptyArray(123)).toBe(false);
+    expect(isEmptyArray('array')).toBe(false);
+    expect(isEmptyArray({})).toBe(false);
+  });
+});
 
 describe('isNonEmptyArray', () => {
   it('should return true if source is a non-empty array', () => {
